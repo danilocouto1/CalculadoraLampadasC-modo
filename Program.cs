fba819797c6929386a2607a1d4764bf0e5d436ed
Danilo <dansantosalfa@gmail.com>
2023-10-17 09:41:22 -0300

    add - Entrada de dados

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        //Entrada de dados
+        Console.WriteLine("Bem vindo a calculadora de lampadas por comodo!");
+        Console.WriteLine("Qual comodo deseja iluminar?");
+        string comodo = Console.ReadLine();
+        Console.WriteLine("Insira a largura do comodo: ");
+        int larguraComodo = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira a comprimento do comodo: ");
+        int comprimentoComodo = int.Parse(Console.ReadLine());
+        Console.WriteLine("Insira quantos watts de potencia tem as lampadas que usara: ");
+        int wattsLampada = int.Parse(Console.ReadLine());
+
+
+    }
+}
